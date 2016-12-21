6ecb902df92439240fa3857affcfb795e3422bce
Broken <whyefa@163.com>
2016-12-21 17:02:56 +0800

    version 0.1.0
    modify

--- RCSwitchDemo/ViewController.swift
+++ RCSwitchDemo/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var aSwitch: RCSwitch!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let aSwitch = RCSwitch(frame: CGRect(x: 100, y: 100, width: 60, height: 25), text: "Abc")
-        self.view.layer.addSublayer(aSwitch.shadowLayer)
+        aSwitch = RCSwitch(frame: CGRect(x: 100, y: 100, width: 60, height: 25))
+        aSwitch.onText = "Abc"
+//        aSwitch.offText = "xyz"
         self.view.addSubview(aSwitch)
+
+
+        let btn = UIButton(type: .custom)
+        btn.setTitle("didi", for: .normal)
+        btn.frame = CGRect(x: 10, y: 300, width: 300, height: 30)
+        btn.addTarget(self, action: #selector(click), for: .touchUpInside)
+        view.addSubview(btn)
+    }
+
+    func click() {
+        aSwitch.onText = "Abc"
     }
 
     override func didReceiveMemoryWarning() {
